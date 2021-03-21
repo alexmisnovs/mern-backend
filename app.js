e38a10e8e3bfb888e7ad7ed78e17ed21aab76e36
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-03-21 12:02:04 +0000

    added middleware to catch all unsupported routes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const HttpError = require("./models/http-error");
 
 const app = express();
 const placesRoutes = require("./routes/places-routes");
@@ -8,6 +8,11 @@ const { json } = require("body-parser");
 app.use(express.json());
 
 app.use("/api/places/v1/", placesRoutes); //places api
+// only if we didn't send the response
+app.use((req, res, next) => {
+  const error = new HttpError("couldnt find this route", 404);
+  next(error);
+});
 
 app.use((error, req, res, next) => {
   if (res.headersSent) {
