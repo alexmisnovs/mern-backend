137f03b88c4241afc7fdd8de7ec2679a858eeda8
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-05-24 17:16:50 +0100

    moved mapbox api to env.. :)

--- utils/location.js
+++ utils/location.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const HttpError = require("../models/http-error");
+require("dotenv").config();
 
 const getCoordsForAddress = async address => {
   let data;
@@ -7,11 +8,9 @@ const getCoordsForAddress = async address => {
     const url = "https://api.mapbox.com/geocoding/v5";
     const endpoint = "mapbox.places";
     const searchText = encodeURIComponent(address);
-    const API_KEY =
-      "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
     const response = await axios({
       method: "GET",
-      url: `${url}/${endpoint}/${searchText}.json/?access_token=${API_KEY}`,
+      url: `${url}/${endpoint}/${searchText}.json/?access_token=${process.env.MAPBOX_API}`,
     });
     data = response.data;
   } catch (e) {
